90133c28b0da3c8b4416cf16432d03dad0184f43
Barbora Risnovska <brisnovs@c2r3s2.42prague.com>
2026-06-11 00:41:36 +0200

    deletes all

--- main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from ui import run_ui
-
-
-if __name__ == "__main__":
-    # read and valitade config - return useful stuff
-    # send validated data into UI
-    run_ui()
